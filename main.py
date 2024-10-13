a5e4d03c338337b186b6c0f7f64644a7a99755bd
Chajrob <husseinn_369@yahoo.com>
2024-10-14 01:15:36 +0300

    Update main.py
    "writting_rules"

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ print('2024-10-12')
 print("It's time for",'2nd commit')
 
 print("Maybe we'll add 3nd commit?")
-print('I'm trying add 1st commit in GitHub')
+print("I'm trying add 1st commit in GitHub")
 
 print('And one more commit...')
